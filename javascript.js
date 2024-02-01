0a79a4130286b7d661f6e6aefbec838d8a2d5d2d
Jeroen Ophof <j.ophof@gmail.com>
2024-02-01 15:55:24 +0100

    Pixels now slowly gain darkness when hovered

--- javascript.js
+++ javascript.js
@@ -1,7 +1,9 @@
 const body = document.querySelector('body');
 const button = document.querySelector('button');
+let pixelArray;
 
 button.addEventListener('click', () => drawBoard());
+
 
 function drawBoard() {
     let gridSize = prompt("Set the gridsize");
@@ -22,21 +24,25 @@ function drawBoard() {
     container.style.width = `${gridSize*10}px`;
     container.style.height = `${gridSize*10}px`;
 
+    pixelArray = [];
     let fragment = new DocumentFragment();
-    for (let i=1; i <= gridSize*gridSize; i++) {
+    for (let i=0; i < gridSize*gridSize; i++) {
         let div = document.createElement('div');
+        pixelArray.push(0);
         div.dataset.number = i;
         div.addEventListener('mouseenter', () => mouseEnter(div));
-
         fragment.appendChild(div);
     }
     container.appendChild(fragment);
 }
 function mouseEnter(div) {
-    div.style.backgroundColor = `rgb(${getRandomInt()},${getRandomInt()},${getRandomInt()})`;
+    let num = div.dataset.number;
+    if (pixelArray[num] < 10) pixelArray[num]++;
+    let value = (pixelArray[num] < 10) ? 255 - pixelArray[num] * Math.floor(255/10) : 0;
+    div.style.backgroundColor = `rgb(${value},${value},${value})`;
 }
 
-function getRandomInt(min = 0, max = 255) {
+function getRandomInt(min = 0, max = 1) {
     return Math.floor(Math.random() * (max - min + 1) + min)
   }
 
